d945fd0bd2a2645f163a2001dfce8bcb64c215c6
Aakash962002 <aakashdesigner96@gmail.com>
2021-08-15 16:15:50 +0530

    done

--- server.js
+++ server.js
@@ -1,64 +1,82 @@
 const express = require("express");
 const { request } = require("http");
 const app = express();
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuid } = require("uuid");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
+  path: "/" 
 });
 
-
-const users ={}
+const port = process.env.PORT || 3030;
+const users ={};
 
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static("/public"));
 app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.redirect(`/room/${uuid()}`);
 });
 
-app.get("/call", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
-});
-
-app.get("/:room", (req, res) => {
-  res.render("room", { room_id: req.params.room });
+app.get("/room/:id", (req, res) => {
+  const { id } = req.params;
+  res.render("room", { roomID: id });
 });
 
 io.on("connection", (socket) => {
+  socket.on("join-room", (roomID, userID, username) => {
+      if (users[roomID]) users[roomID].push({ id: userID, name: username, video: true, audio: true });
+      else users[roomID] = [{ id: userID, name: username, video: true, audio: true }];
 
-  socket.on('new-chat-user',user => {
-    users[socket.id] = user;
-    socket.broadcast.emit('user-connected-chat',user);
-    console.log(user);
-  });
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId);
-    socket.broadcast.to(roomId).emit("user-connected", userId);
+      socket.join(roomID);
+      socket.broadcast.to(roomID).emit("user-connected", userID, username);
 
-    
-  });
-  socket.on("SentMessage", message => {
-    io.emit("createMessage",{ message: message, name:users[socket.id] });
-  });
+      socket.on("message", (message) => {
+          io.in(roomID).emit("message", message, userID, username);
+      });
 
-  
-  
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id]);
-    
-      delete users[socket.id]
-    
-    
+      io.in(roomID).emit("participants", users[roomID]);
+
+      socket.on("mute-mic", () => {
+          users[roomID].forEach((user) => {
+              if (user.id === userID) return (user.audio = false);
+          });
+          io.in(roomID).emit("participants", users[roomID]);
+      });
+
+      socket.on("unmute-mic", () => {
+          users[roomID].forEach((user) => {
+              if (user.id === userID) return (user.audio = true);
+          });
+          io.in(roomID).emit("participants", users[roomID]);
+      });
+
+      socket.on("stop-video", () => {
+          users[roomID].forEach((user) => {
+              if (user.id === userID) return (user.video = false);
+          });
+          io.in(roomID).emit("participants", users[roomID]);
+      });
+
+      socket.on("play-video", () => {
+          users[roomID].forEach((user) => {
+              if (user.id === userID) return (user.video = true);
+          });
+          io.in(roomID).emit("participants", users[roomID]);
+      });
+
+      socket.on("disconnect", () => {
+          socket.broadcast.to(roomID).emit("user-disconnected", userID, username);
+          users[roomID] = users[roomID].filter((user) => user.id !== userID);
+          if (users[roomID].length === 0) delete users[roomID];
+          else io.in(roomID).emit("participants", users[roomID]);
+      });
   });
-  
 });
 
-
-
-server.listen(process.env.PORT || 3030);
+server.listen(port, () => console.log("App is listening on port", port));
